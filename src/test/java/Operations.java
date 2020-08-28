8acc692d869a3aff701ac5effe986121dedfd18b
Shimon Magal <shimon.magal@gmail.com>
2020-08-28 23:50:19 +0300

    add basic create

--- src/test/java/Operations.java
+++ src/test/java/Operations.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public enum Operations {
+public enum Operations {
+    CREATE("CREATE TABLE IF NOT EXISTS $QUEUE");
+
+    private final String sqlTemplate;
+
+    private Operations(String sqlTeamplate)
+    {
+        this.sqlTemplate = sqlTeamplate;
+    }
+
+    String bindQueueName(String queueName)
+    {
+        return sqlTemplate.replace("$QUEUE", queueName);
+    }
 }
