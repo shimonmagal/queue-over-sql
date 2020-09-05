012643e3b080c7a465963a205baf59f18a67c9e0
Shimon Magal <shimon.magal@gmail.com>
2020-09-05 20:52:07 +0300

    fix some errors

--- src/test/java/QueueOverSqlTests.java
+++ src/test/java/QueueOverSqlTests.java
@@ -1,10 +1,6 @@
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 public class QueueOverSqlTests {
