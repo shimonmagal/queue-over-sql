2a6a6ab1b7ac8eb825ef82923259f354617e36a9
Shimon Magal <shimon.magal@gmail.com>
2020-09-05 20:50:57 +0300

    testing

--- src/test/java/QueueOverSqlTests.java
+++ src/test/java/QueueOverSqlTests.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class QueueOverSqlTests {
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+
+public class QueueOverSqlTests {
+    private final static String jdbcUrl = "jdbc:h2:mem:queueOverSqlTest";
+
+
+    private
+
+    @Test
+    public void test()
+    {
+        new QueueOverSql(jdbcUrl, 30, TimeUnit.MINUTES, 20, TimeUnit.SECONDS);
+
+        Assertions.assertTrue(true);
+    }
 }
