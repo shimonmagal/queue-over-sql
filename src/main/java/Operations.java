cc4f0c8b967ff6ef18ad58c662a63b5844e989d5
Shimon Magal <shimon.magal@gmail.com>
2020-10-04 21:55:19 +0300

    add connection

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -1,5 +1,11 @@
 public enum Operations {
-    CREATE("CREATE TABLE IF NOT EXISTS $QUEUE"),
+    CREATE("CREATE TABLE IF NOT EXISTS $QUEUE(" +
+            "id long NOT NULL," +
+            "message VARCHAR(10000)," +
+            "consumer_id VARCHAR(128)," +
+            "publish_time long," +
+            "consume_time long," +
+            "ttl long)"),
     PUBLISH("INSERT INTO $QUEUE values(?,?)"),
     DELETE("DELETE FROM $QUEUE where messageId=?"),
     CONSUME(""),
