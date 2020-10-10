2bbe2b3d3df1a2b00052b015a9a2644c8afece7c
Shimon Magal <shimon.magal@gmail.com>
2020-10-10 22:31:09 +0300

    fix compilation issue

--- src/main/java/QueueOverSql.java
+++ src/main/java/QueueOverSql.java
@@ -69,9 +69,7 @@ public class QueueOverSql {
         }
         catch (SQLException e)
         {
-            e.printStackTrace();
-
-            logger.
+            logger.error("SQL operation", e);
             return false;
         }
 
