634e50c561960d6b0e45c54fff0fa736f1d61da6
Shimon Magal <shimon.magal@gmail.com>
2020-10-16 23:29:04 +0300

    insert

--- src/main/java/com/queueoversql/Operations.java
+++ src/main/java/com/queueoversql/Operations.java
@@ -8,7 +8,7 @@ public enum Operations {
             "publish_time long," +
             "consume_time long," +
             "ttl long)"),
-    PUBLISH("INSERT INTO $QUEUE (id, message) values(?,?)"),
+    PUBLISH("INSERT INTO $QUEUE (id, message, publish_time) values(?,?,?)"),
     DELETE("DELETE FROM $QUEUE where messageId=?"),
     CONSUME(""),
     UNASSIGN_TIMEDOUT("");
