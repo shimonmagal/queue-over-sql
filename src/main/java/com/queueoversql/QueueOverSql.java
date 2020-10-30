2e48229bb86cf59872c9ff3f107301cffd2b69a5
Shimon Magal <shimon.magal@gmail.com>
2020-10-30 20:11:12 +0200

    add support for consumption

--- src/main/java/com/queueoversql/QueueOverSql.java
+++ src/main/java/com/queueoversql/QueueOverSql.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -18,8 +19,7 @@ public class QueueOverSql {
     private final long consumerRound;
 
     public QueueOverSql(String jdbcUrl, long messageTimeout, TimeUnit messageTimeoutUnit,
-                        long backgroundThreadInterval, TimeUnit backgroundThreadIntervalUnit)
-    {
+                        long backgroundThreadInterval, TimeUnit backgroundThreadIntervalUnit) {
         this.jdbcUrl = jdbcUrl;
         this.messageTimeoutMillis = TimeUnit.MILLISECONDS.convert(messageTimeout, messageTimeoutUnit);
         this.backgroundThreadInterval = TimeUnit.MILLISECONDS.convert(backgroundThreadInterval, backgroundThreadIntervalUnit);
@@ -28,13 +28,11 @@ public class QueueOverSql {
         this.consumerRound = 0l;
     }
 
-    public boolean createQueue(String queueName)
-    {
+    public boolean createQueue(String queueName) {
         logger.debug("Creating queue {}", queueName);
         boolean result = executeWithParams(Operations.CREATE.bindQueueName(queueName));
 
-        for (String field : Operations.fieldsForIndexing)
-        {
+        for (String field : Operations.fieldsForIndexing) {
             String sql = Operations.INDEX.bindQueueAndFieldName(queueName, field);
 
             result &= executeWithParams(sql);
@@ -43,34 +41,35 @@ public class QueueOverSql {
         return result;
     }
 
-    public Long publishTask(String queueName, String message)
-    {
+    public Long publishTask(String queueName, String message) {
         long messageId = UUID.randomUUID().getLeastSignificantBits();
         logger.debug("Publishing new task with id {} to queue {}", messageId, queueName);
 
         boolean result = executeWithParams(Operations.PUBLISH.bindQueueName(queueName), messageId, message, System.currentTimeMillis());
 
-        if (!result)
-        {
+        if (!result) {
             return null;
         }
 
         return messageId;
     }
 
-    public boolean deleteTask(String queueName, long messageId)
-    {
+    public boolean deleteTask(String queueName, long messageId) {
         logger.debug("Deleting task with id {} from queue {}", messageId, queueName);
 
         return executeWithParams(Operations.DELETE.bindQueueName(queueName), messageId);
     }
 
-    public List<Task> consume(String queueName, int count)
-    {
+    public List<Task> consume(String queueName, int count) {
         return null;
     }
 
     private boolean executeWithParams(String sql, Object... params)
+    {
+        executeWithParams(sql, new LinkedList<>(), params);
+    }
+
+    private boolean executeWithParams(String sql, List<Task> tasks, Object... params)
     {
         try (Connection connection = DriverManager.getConnection(jdbcUrl);)
         {
@@ -87,7 +86,7 @@ public class QueueOverSql {
                     {
                         while (rs.next())
                         {
-
+                            // tasks
                         }
                     }
                 }
